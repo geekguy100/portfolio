0b3bd030f57185ea83679caac8dcacafa1e0b34f
Kyle Grenier <kyle.grenier11@gmail.com>
2026-04-25 09:22:39 -0700

    Simple about me display and project showcase

---
+++ app/_aboutme/page.tsx
@@ -0,0 +1,26 @@
+export default function AboutMe() {
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="space-y-4">
+        <h3 className="text-center">About Me</h3>
+        <p>
+          Hey there! I'm Kyle Grenier, a graduate of Bradley University and an immersive technology developer for The
+          Aerospace Corporation based in South California.
+        </p>
+        <p>
+          Ever since I was young, I have loved technology in all aspects, from robotics and science fiction to computers
+          and programming. I'm the go-to IT person in my house, and as a triplet, I am definitely the most curious of
+          the three.
+        </p>
+        <p>
+          It was in elementary school that I started tinkering around with game development in GameMaker, and in middle
+          school more advanced programming and the Unity game engine. For the longest time, I've known that I wanted to
+          go into the video game development industry. I love seeing my work evolve over time and the reactions people
+          have when playing my games.
+        </p>
+        <p>Although programming is my specialty, I also enjoy creating electronic music in my free time.</p>
+        <p>Take a look at the work I've produced during my time as a student.</p>
+      </div>
+    </div>
+  )
+}
