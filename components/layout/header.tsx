b4c54f75f138bf441fec44a42f04c42a4945ad26
Kyle Grenier <kyle.grenier11@gmail.com>
2026-04-25 16:38:49 -0700

    Added hat logo to header

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,8 +1,19 @@
 import { NavigationBar } from "./nav-bar"
+import Image from "next/image"
 
 export function SiteHeader() {
   return (
     <header className="flex flex-col items-center justify-between gap-4 bg-card py-4 text-center">
+      <Image
+        unoptimized
+        src="/hat.png"
+        width={175}
+        height={30}
+        preload
+        className="absolute -top-4 left-110 animate-header-enter"
+        style={{ animationComposition: "add" }}
+        alt="A black hat with wings"
+      />
       <div>
         <h1 className="text-4xl">Kyle Grenier</h1>
         <h4>Immersive Technology and Game Developer</h4>
