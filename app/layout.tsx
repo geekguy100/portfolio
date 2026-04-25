9be4b7752e44fd104c7469a9b0a962023c059b44
Kyle Grenier <kyle.grenier11@gmail.com>
2026-04-25 09:21:15 -0700

    Changes to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
-import { Geist, Geist_Mono, Roboto } from "next/font/google"
+import { Geist_Mono, Roboto } from "next/font/google"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
+import { NavigationBar } from "@/components/layout/nav-bar"
 
-const roboto = Roboto({subsets:['latin'],variable:'--font-sans'})
+const roboto = Roboto({ subsets: ["latin"], variable: "--font-sans" })
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
@@ -23,7 +24,14 @@ export default function RootLayout({
       className={cn("antialiased", fontMono.variable, "font-sans", roboto.variable)}
     >
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          <header className="bg-gray-500">
+            <h1>Kyle Grenier</h1>
+            <p>Immersive Technology and Game Developer</p>
+          </header>
+          <NavigationBar />
+          <main>{children}</main>
+        </ThemeProvider>
       </body>
     </html>
   )
