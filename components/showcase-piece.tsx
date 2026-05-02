bc5594aa87a947d7659b12aef4ad31c97e164c81
Kyle Grenier <kyle.grenier11@gmail.com>
2026-05-01 22:13:06 -0700

    Minor styling changes, determining href from provided project ID

--- components/showcase-piece.tsx
+++ components/showcase-piece.tsx
@@ -4,14 +4,14 @@ import Link from "next/link"
 export interface ShowcasePieceProps {
   title: string
   descriptions: string[]
-  href: string
+  id: string
   img: string
 }
 
-export function ShowcasePiece({ title, descriptions, href, img }: ShowcasePieceProps) {
+export function ShowcasePiece({ title, descriptions, id, img }: ShowcasePieceProps) {
   return (
-    <Link href={href}>
-      <div className="flex gap-4 rounded-md p-6">
+    <Link href={`/projects/${id}`}>
+      <div className="flex gap-4 rounded-md p-6 outline-primary hover:outline-3">
         <Image
           className="object-contain"
           unoptimized
@@ -20,7 +20,7 @@ export function ShowcasePiece({ title, descriptions, href, img }: ShowcasePieceP
           height={160}
           alt={`Thumbnail for content titled "${title}"`}
         />
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col">
           <h3>{title}</h3>
           {descriptions.map((desc, i) => (
             <p key={i}>{desc}</p>
