fa2c2f0ec3c62cd87b016dc6dcff1e6a8aa02926
Kyle Grenier <kyle.grenier11@gmail.com>
2026-05-01 19:04:07 -0700

    Fixed hrefs in nav-bar

--- components/layout/nav-bar.tsx
+++ components/layout/nav-bar.tsx
@@ -2,8 +2,8 @@ import { ButtonGroup } from "../ui/button-group"
 import { NavigationButton } from "./nav-button"
 
 const routes = [
-  { href: "#aboutme", content: "Home" },
-  { href: "#projects", content: "Projects" },
+  { href: "/", content: "Home" },
+  { href: "/#projects", content: "Projects" },
 ]
 
 export function NavigationBar() {
