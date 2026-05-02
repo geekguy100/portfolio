15ea1cdb1b37f271e2644cb3efb617580725ddb5
Kyle Grenier <kyle.grenier11@gmail.com>
2026-05-01 22:07:37 -0700

    Created a generic Container component, using it for the other concrete containers that are exported, removed DivSection component

--- components/containers.tsx
+++ components/containers.tsx
@@ -1,28 +1,22 @@
 import { cn } from "@/lib/utils"
 import type { ComponentProps, HTMLAttributes } from "react"
 
-const COMMON_CLASSNAME: HTMLAttributes<"div">["className"] = "site-section flex flex-col gap-2 p-6"
+const COMMON_CLASSNAME: HTMLAttributes<"div">["className"] = "site-section flex flex-col p-6"
 
-export function Section({ children, id, className, ...props }: ComponentProps<"section">) {
-  return (
-    <section {...props} className={cn(COMMON_CLASSNAME, className)} id={id}>
-      {children}
-    </section>
-  )
+export function Section(props: ComponentProps<"section">) {
+  return <Container type="section" {...props} />
 }
 
 export function Article({ children, id, className, ...props }: ComponentProps<"article">) {
-  return (
-    <article {...props} className={cn(COMMON_CLASSNAME, className)} id={id}>
-      {children}
-    </article>
-  )
+  return <Container type="article" {...props} />
 }
 
-export function DivSection({ children, id, className, ...props }: ComponentProps<"div">) {
-  return (
-    <div {...props} className={cn(COMMON_CLASSNAME, className)} id={id}>
-      {children}
-    </div>
-  )
+export function Container<T extends keyof React.JSX.IntrinsicElements>({
+  type,
+  className,
+  ...props
+}: ComponentProps<T> & { type: T }) {
+  // Have to bypass some TypeScript here to prevent errors, but React allows any intrinsic element provided as a string to be used as a component.
+  const Container = type as unknown as "div"
+  return <Container className={cn(COMMON_CLASSNAME, className)} {...(props as unknown as ComponentProps<"div">)} />
 }
