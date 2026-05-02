58ed218e0b9c81a0fda6dfc2a604a678a29f5585
Kyle Grenier <kyle.grenier11@gmail.com>
2026-05-02 16:32:05 -0700

    Created awaitPromise helper function

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -1,14 +1,19 @@
-import type { Metadata } from "next";
-import {content} from "@/misc/showcase.json"
+import type { Metadata } from "next"
+import { content } from "@/misc/showcase.json"
 
-export function getProjectMetdata(projectId: string): Metadata
-{
-    const project = content.find(t => t.id === projectId);
-    if (project === undefined) throw new Error("Could not find project with ID " + projectId);
+export function getProjectMetdata(projectId: string): Metadata {
+  const project = content.find((t) => t.id === projectId)
+  if (project === undefined) throw new Error("Could not find project with ID " + projectId)
 
-    return {
-        title: `${project.title} | Kyle Grenier`,
-        authors: [{name: "Kyle Grenier"}],
-        description: project.descriptions.join(" ")
-    }
+  return {
+    title: `${project.title} | Kyle Grenier`,
+    authors: [{ name: "Kyle Grenier" }],
+    description: project.descriptions.join(" "),
+  }
+}
+
+export function awaitPromise(ms: number): Promise<void> {
+  return new Promise<void>((resolve) => {
+    setTimeout(resolve, ms)
+  })
 }
