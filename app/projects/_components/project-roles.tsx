20924d1c9a0dd117b65f6ca5cee10af8e7df8869
Kyle Grenier <kyle.grenier11@gmail.com>
2026-05-02 09:15:37 -0700

    Added in project roles to DGolf page

--- app/projects/_components/project-roles.tsx
+++ app/projects/_components/project-roles.tsx
@@ -2,10 +2,10 @@ import { SectionTitle } from "@/components/section-title"
 import { cn } from "@/lib/utils"
 import type { ComponentPropsWithoutRef, PropsWithChildren } from "react"
 
-export function ProjectRoles({ children }: PropsWithChildren) {
+export function ProjectRoles({ title, children }: PropsWithChildren & { title?: string }) {
   return (
     <article>
-      <SectionTitle>My Roles</SectionTitle>
+      <SectionTitle>{title ?? "My Roles"}</SectionTitle>
       <div className="flex flex-col gap-2 text-left">{children}</div>
     </article>
   )
@@ -16,11 +16,11 @@ export function ProjectRole({ children }: PropsWithChildren) {
 }
 
 export function ProjectRoleHeader({ className, children, ...props }: ComponentPropsWithoutRef<"h3">) {
-  return <h4 className={cn("text-center underline", className)}>{children}</h4>
+  return <h4 className={cn("underline", className)}>{children}</h4>
 }
 
-export function ProjectRoleResponsibilities(props: ComponentPropsWithoutRef<"ul">) {
-  return <ul {...props} />
+export function ProjectRoleResponsibilities({ className, ...props }: ComponentPropsWithoutRef<"ul">) {
+  return <ul className={cn("list-disc [&_ul]:ml-4 [&_ul]:list-[circle]", className)} {...props} />
 }
 
 export function ProjectRoleResponsibility(props: ComponentPropsWithoutRef<"li">) {
