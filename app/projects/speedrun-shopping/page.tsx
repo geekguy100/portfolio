4d8723e6d241fe6e0a788e90811945a13617a1c3
Kyle Grenier <kyle.grenier11@gmail.com>
2026-05-03 13:07:04 -0700

    Added project footer component, added a footer to speedrun shopping

--- app/projects/speedrun-shopping/page.tsx
+++ app/projects/speedrun-shopping/page.tsx
@@ -4,6 +4,8 @@ import { ProjectIntro, ProjectTitle, ProjectDescription } from "../_components/p
 import { Project } from "../_components/project-wrapper"
 import { PdfViewer } from "@/components/pdf-viewing/pdf-viewer-lazy"
 import { SectionTitle } from "@/components/section-title"
+import { ProjectFooter } from "../_components/project-footer"
+import Link from "next/link"
 
 export const metadata = getProjectMetdata("speedrun-shopping")
 
@@ -39,6 +41,19 @@ export default function SpeedrunShoppingPage() {
           src="/speedrunshopping_docs.pdf"
         />
       </article>
+      <ProjectFooter>
+        <p>
+          For a full list of documents, forms, assets, etc., please click{" "}
+          <Link
+            href="https://drive.google.com/drive/u/0/folders/1c30YEPJtQDrso8lM7uSEHH6bER8n0z2q"
+            target="_blank"
+            className="underline"
+          >
+            here
+          </Link>
+          .
+        </p>
+      </ProjectFooter>
     </Project>
   )
 }
