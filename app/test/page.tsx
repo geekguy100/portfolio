53e01aa95951efa188841b82bd577c5452abe994
Kyle Grenier <kyle.grenier11@gmail.com>
2026-05-08 15:35:31 -0700

    Testing stuff on test page

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -2,8 +2,13 @@ import { PdfViewer } from "@/components/pdf-viewing/pdf-viewer-lazy"
 
 export default function Page() {
   return (
-    <div className="sm:bg-green-500 md:bg-red-500 lg:bg-orange-500">
-      <PdfViewer src="/dgcodedocs.pdf" />
+    <div className="mx-12">
+      <p>Hey</p>
     </div>
   )
+  // return (
+  //   <div className="sm:bg-green-500 md:bg-red-500 lg:bg-orange-500">
+  //     <PdfViewer src="/dgcodedocs.pdf" />
+  //   </div>
+  // )
 }
