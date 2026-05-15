37a46887605257e523daf9db537ee9d98090343a
Kyle Grenier <kyle.grenier11@gmail.com>
2026-05-14 22:41:16 -0700

    chore: changed ShowcasePiece animation to be easier on the eyes

--- components/showcase-piece.tsx
+++ components/showcase-piece.tsx
@@ -1,7 +1,6 @@
 "use client"
-import { childSlideInVariants, containerSlideInVariants, slideInProps } from "@/lib/animation"
+import { childSlideInVariants, containerSlideInVariants } from "@/lib/animation"
 import { motion } from "motion/react"
-import Image from "next/image"
 import Link from "next/link"
 import SlideInContainer from "./slide-in-container"
 
@@ -18,7 +17,16 @@ export function ShowcasePiece({ title, descriptions, id, img, animIndex }: Showc
     <Link href={`/projects/${id}`}>
       <article className="flex flex-col gap-4 rounded-md p-6 outline-primary hover:outline-3 sm:flex-row">
         <div className="relative aspect-300/160 size-full max-h-40 max-w-75 flex-none self-center max-sm:mx-auto">
-          <Image fill unoptimized loading="eager" src={img} alt={`Thumbnail for content titled "${title}"`} />
+          <motion.img
+            variants={childSlideInVariants}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            loading="eager"
+            src={img}
+            className="size-full"
+            alt={`Thumbnail for content titled "${title}"`}
+          />
         </div>
 
         <SlideInContainer>
@@ -27,7 +35,7 @@ export function ShowcasePiece({ title, descriptions, id, img, animIndex }: Showc
           </motion.h3>
           {descriptions.map((desc, i) => {
             return (
-              <motion.p variants={childSlideInVariants} key={i}>
+              <motion.p variants={containerSlideInVariants} key={i}>
                 {desc}
               </motion.p>
             )
