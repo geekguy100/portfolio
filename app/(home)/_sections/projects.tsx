0e530cb5a17d6b0ed112bfaabe0805f57086e7ae
Kyle Grenier <kyle.grenier11@gmail.com>
2026-05-15 18:07:34 -0700

    fix: fixed bug causing descriptions of ArticleWithIntro components to not play again once they re-mount

--- app/(home)/_sections/projects.tsx
+++ app/(home)/_sections/projects.tsx
@@ -2,9 +2,11 @@
 import { ArticleWithIntro } from "@/components/section-with-intro"
 import { ProjectTabs, type Section } from "../_components/project-tabs"
 import { useCallback, useState, type ReactNode } from "react"
+import { MotionDiv, MotionParagraph } from "@/components/motion-primitives"
+import { childFadeInVariants } from "@/lib/animation"
 
 const descriptions: Record<Section, ReactNode> = {
-  university: "Projects I worked on during my time in university",
+  university: <UniversityDescription />,
   aerospace: <AerospaceDescription />,
 }
 
@@ -14,21 +16,27 @@ export default function ProjectsShowcase() {
   const setDescriptionFromTab = useCallback((tab: Section) => setDescription(descriptions[tab]), [])
 
   return (
-    <ArticleWithIntro id="projects" title="Projects" description={description}>
+    <ArticleWithIntro animate="visible" id="projects" title="Projects" description={description}>
       <ProjectTabs defaultValue={DEFAULT_SECTION} onTabChanged={setDescriptionFromTab} />
     </ArticleWithIntro>
   )
 }
 
+function UniversityDescription() {
+  return (
+    <MotionParagraph variants={childFadeInVariants}>Projects I worked on during my time in university</MotionParagraph>
+  )
+}
+
 function AerospaceDescription() {
   return (
-    <div>
+    <MotionDiv variants={childFadeInVariants}>
       <p>Projects I worked on during my time at The Aerospace Corporation</p>
       <div className="flex justify-center gap-2">
         <span>-</span>
         <p className="italic">Please note that due to the nature of the work conducted, information may be limited</p>
         <span>-</span>
       </div>
-    </div>
+    </MotionDiv>
   )
 }
