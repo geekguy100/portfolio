57be31198dba3b097e981f602144b2d2d9fdac09
Kyle Grenier <kyle.grenier11@gmail.com>
2026-05-17 20:10:15 -0700

    chore: renamed useMobile hook to useScreenBreakpoint for flexability in usage, removed .gitkeep file in hooks folder

--- hooks/use-mobile.tsx
+++ hooks/use-mobile.tsx
@@ -2,18 +2,18 @@
 
 import * as React from "react"
 
-export function useIsMobile(mobileBreakpoint = 768) {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+export function useScreenBreakpoint(breakpoint = 768) {
+  const [hitBreakpoint, setHitBreakpoint] = React.useState<boolean | undefined>(undefined)
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${mobileBreakpoint - 1}px)`)
+    const mql = window.matchMedia(`(max-width: ${breakpoint - 1}px)`)
     const onChange = () => {
-      setIsMobile(window.innerWidth < mobileBreakpoint)
+      setHitBreakpoint(window.innerWidth < breakpoint)
     }
     mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < mobileBreakpoint)
+    setHitBreakpoint(window.innerWidth < breakpoint)
     return () => mql.removeEventListener("change", onChange)
-  }, [mobileBreakpoint])
+  }, [breakpoint])
 
-  return !!isMobile
+  return !!hitBreakpoint
 }
